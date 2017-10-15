d82d0a1d96d9296bf3444272ec0e3bcb167d961c
HustonMavr <huston.mavr@gmail.com>
2017-10-15 18:34:35 +0300

    trying to delete old files

--- src/main/java/lastunion/application/Views/AbstractView.java
+++
@@ -1,9 +0,0 @@
-package lastunion.application.views;
-
-public interface AbstractView {
-    @SuppressWarnings("unused")
-    boolean isFilled();
-
-    @SuppressWarnings("unused")
-    boolean isValid();
-}
