1372c66a4cb9825918be1d6e426ad1cc36ae7639
HustonMavr <huston.mavr@gmail.com>
2017-10-19 14:10:47 +0300

    refactoring1

--- src/test/java/tests/api/ChangePaswordIntTest.java
+++ src/test/java/tests/api/ChangePaswordIntTest.java
@@ -22,9 +22,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
 @AutoConfigureMockMvc(print = MockMvcPrint.NONE)
-@Category(tests.IntegrationTest.class)
-//
-
 public class ChangePaswordIntTest {
     @Autowired
     private MockMvc mock;
@@ -58,7 +55,7 @@ public class ChangePaswordIntTest {
 
     @SuppressWarnings("ThrowInsideCatchBlockWhichIgnoresCaughtException")
     @Before
-    public void setUp(){
+    public void setUp() throws Exception {
         userName = faker.name().username();
         userEmail = faker.internet().emailAddress();
         userPassword = faker.internet().password();
