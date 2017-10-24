8402a516b3bb5c3b357c6f55278954480538b8dc
strelnikoff <strelnikoff@github.com>
2017-10-24 14:24:01 +0300

    добавлено автоматическое переключения языков

--- src/main/java/lastunion/application/controllers/SignInController.java
+++ src/main/java/lastunion/application/controllers/SignInController.java
@@ -30,18 +30,18 @@ public class SignInController {
     @RequestMapping(path = "/api/user/signin", method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE
     )
-    public ResponseEntity<ResponseCode> signIn(@RequestBody SignInView signInView, HttpSession httpSession) {
+    public ResponseEntity<ResponseCode> signIn(Locale locale, @RequestBody SignInView signInView, HttpSession httpSession) {
 
         if (!signInView.isFilled()) {
             return new ResponseEntity<>(new ResponseCode(false,
-                    messageSource.getMessage("msgs.bad_request_json", null, Locale.ENGLISH)),
+                    messageSource.getMessage("msgs.bad_request_json", null, locale)),
                     HttpStatus.BAD_REQUEST);
         }
 
         // Incorrect authenticatiion data
         if (!signInView.isValid()) {
             return new ResponseEntity<>(new ResponseCode(false,
-                    messageSource.getMessage("msgs.bad_request_form", null, Locale.ENGLISH)),
+                    messageSource.getMessage("msgs.bad_request_form", null, locale)),
                     HttpStatus.BAD_REQUEST);
         }
         final SignInModel signInUser = new SignInModel(signInView.getUserName(), signInView.getUserPassword());
@@ -53,18 +53,18 @@ public class SignInController {
             case INCORRECT_LOGIN:
             case INCORRECT_PASSWORD:
                 return new ResponseEntity<>(new ResponseCode(false,
-                        messageSource.getMessage("msgs.forbidden", null, Locale.ENGLISH)),
+                        messageSource.getMessage("msgs.forbidden", null, locale)),
                         HttpStatus.FORBIDDEN);
 
             case OK:
                 httpSession.setAttribute("userName", signInView.getUserName());
                 return new ResponseEntity<>(new ResponseCode(true,
-                        messageSource.getMessage("msgs.ok", null, Locale.ENGLISH)),
+                        messageSource.getMessage("msgs.ok", null, locale)),
                         HttpStatus.OK);
 
             default:
                 return new ResponseEntity<>(new ResponseCode(false,
-                        messageSource.getMessage("msgs.internal_server_error", null, Locale.ENGLISH)),
+                        messageSource.getMessage("msgs.internal_server_error", null, locale)),
                         HttpStatus.INTERNAL_SERVER_ERROR);
 
         }
--- src/main/java/lastunion/application/controllers/SignUpController.java
+++ src/main/java/lastunion/application/controllers/SignUpController.java
@@ -29,17 +29,17 @@ public class SignUpController {
 
     @RequestMapping(path = "/api/user/signup", method = RequestMethod.POST,
         produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<ResponseCode> signUp(@RequestBody SignUpView signUpView, HttpSession httpSession) {
+    public ResponseEntity<ResponseCode> signUp(Locale locale, @RequestBody SignUpView signUpView, HttpSession httpSession) {
 
         if (!signUpView.isFilled()) {
             return new ResponseEntity<>(new ResponseCode(false,
-                messageSource.getMessage("msgs.bad_request_json", null, Locale.ENGLISH)),
+                messageSource.getMessage("msgs.bad_request_json", null, locale)),
                 HttpStatus.BAD_REQUEST);
         }
         // Incorrect reg data
         if (!signUpView.isValid()) {
             return new ResponseEntity<>(new ResponseCode(false,
-                messageSource.getMessage("msgs.bad_request_form", null, Locale.ENGLISH)),
+                messageSource.getMessage("msgs.bad_request_form", null, locale)),
                 HttpStatus.BAD_REQUEST);
         }
 
@@ -52,22 +52,22 @@ public class SignUpController {
             case OK:
                 httpSession.setAttribute("userName", signUpView.getUserName());
                 return new ResponseEntity<>(new ResponseCode(true,
-                    messageSource.getMessage("msgs.created", null, Locale.ENGLISH)),
+                    messageSource.getMessage("msgs.created", null, locale)),
                     HttpStatus.CREATED);
 
             case LOGIN_IS_TAKEN:
                 return new ResponseEntity<>(new ResponseCode(false,
-                    messageSource.getMessage("msgs.conflict_login", null, Locale.ENGLISH)),
+                    messageSource.getMessage("msgs.conflict_login", null, locale)),
                     HttpStatus.CONFLICT);
 
             case EMAIL_IS_TAKEN:
                 return new ResponseEntity<>(new ResponseCode(false,
-                        messageSource.getMessage("msgs.conflict_email", null, Locale.ENGLISH)),
+                        messageSource.getMessage("msgs.conflict_email", null, locale)),
                         HttpStatus.CONFLICT);
 
             default:
                 return new ResponseEntity<>(new ResponseCode(false,
-                    messageSource.getMessage("msgs.internal_server_error", null, Locale.ENGLISH)),
+                    messageSource.getMessage("msgs.internal_server_error", null, locale)),
                     HttpStatus.INTERNAL_SERVER_ERROR);
 
         }
